e1c173ee10456f4fb9d08dc50dd7c433475b531f
Vacheto1986 <128089859+Vacheto1986@users.noreply.github.com>
2025-01-16 16:03:08 +0200

    add 404 page add create post page

--- src/index.js
+++ src/index.js
@@ -14,7 +14,19 @@ app.set('views', 'src/views')
 app.use('/static', express.static('src/public'));
 
 app.get('/', (req, res) => {
-    res.render('home', { layout: false });
+    res.render('home');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.get('*', (req, res) => {
+    res.render('404');
+})
 
 app.listen(5000, () => console.log('Server is listening on http"//localhost:5000...'));
