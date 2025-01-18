4de20bec399173b10ac2ddc6ca95049e490485b7
Vacheto1986 <128089859+Vacheto1986@users.noreply.github.com>
2025-01-18 13:00:58 +0200

    add movieController and create page

---
+++ src/controllers/homeController.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import movies from '../movies.js'
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('home', { movies });
+});
+
+// router.get('/about', (req, res) => {
+//     res.render('about');
+// });
+
+// router.get('/search', (req, res) => {
+//     res.render('search')
+// });
+
+export default router;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import homeController from './controllers/homeControler.js';
 import routes from './routes.js';
 
 const app = express();
@@ -13,7 +12,6 @@ app.set('view engine', 'hbs');
 app.set('views', 'src/views')
 
 app.use('/static', express.static('src/public'));
-app.use(homeController);
 app.use(routes);
 
 app.listen(5000, () => console.log('Server is listening on http"//localhost:5000...'));
