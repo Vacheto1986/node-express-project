9545220829ad482335562b892fd59353abb45beb
Vacheto1986 <128089859+Vacheto1986@users.noreply.github.com>
2025-01-23 07:48:02 +0200

    setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import routes from './routes.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
+//db configuration
+try {
+    const uri = 'mongodb://localhost:27017/magic-movies'
+    await mongoose.connect(uri);
+
+    console.log('DB connected !!!');
+    
+} catch (error) {
+    console.error(error.message)
+}
+
+//handlebars configuration
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
-
 app.set('view engine', 'hbs');
 app.set('views', 'src/views')
 
+
+//express configuration
 app.use('/static', express.static('src/public'));
 app.use(express.urlencoded({ extended: false })); //teach express to parse from data
 app.use(routes);
 
-app.listen(5000, () => console.log('Server is listening on http"//localhost:5000...'));
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
